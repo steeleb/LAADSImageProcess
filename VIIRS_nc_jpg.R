7ddd53e7ac0c327b3ab9a8facde22fcdc91aebb5
B Steele <steeleb@Bs-MacBook-Air.local>
2022-12-28 16:43:46 -0700

    work on R workflow, py was not displaying correctly

---
+++ VIIRS_nc_jpg.R
@@ -0,0 +1,62 @@
+library(ncdf4)
+library(jpeg)
+library(tidyverse)
+
+#point to directory
+banddir = '/Users/steeleb/OneDrive - Colostate/Superior/images/LAADS_VIIRS_band/'
+geodir = '/Users/steeleb/OneDrive - Colostate/Superior/images/LAADS_VIIRS_geo/'
+dumpdir = '/Users/steeleb/OneDrive - Colostate/Superior/images/LAADS_VIIRS_jpeg'
+
+#get a list of files here
+band_files = list.files(banddir, pattern = '.nc')
+
+#get the date and time from these to match with geo dir
+datetimelist = substr(band_files, 11, 22)
+
+#get the list of geo files
+geo_files = list.files(geodir, pattern = '.nc')
+#only list the ones that match
+geo_files = geo_files[substr(geo_files, 11, 22)==datetimelist]
+
+
+#############################
+## grab band and geo files and metadata for file naming
+date = datetimelist[1]
+
+#for now, work with one file
+bandfile = file.path(banddir, band_files[grepl(datetimelist[1], band_files)])
+geofile = file.path(geodir, geo_files[grepl(datetimelist[1], geo_files)])
+
+#read in as netcdf; xarray will not recognize the dimensions
+vi_band = nc_open(bandfile)
+vi_geo = nc_open(geofile)
+
+#get coordinates from global attributes of geo file
+latitude <- ncvar_get(vi_geo, 'geolocation_data/latitude')
+longitude <- ncvar_get(vi_geo, 'geolocation_data/longitude')
+
+#get short product name from global attributes of band data
+name <- ncatt_get(vi_band, 0)$ShortName
+
+## create RGB images 
+#store the band group in it's own object
+tc_red = ncvar_get(vi_band, 'observation_data/M05')
+tc_green = ncvar_get(vi_band, 'observation_data/M04')
+tc_blue = ncvar_get(vi_band, 'observation_data/M03')
+nc_red = ncvar_get(vi_band, 'observation_data/M10')
+nc_green = ncvar_get(vi_band, 'observation_data/M07')
+nc_blue = ncvar_get(vi_band, 'observation_data/M05')
+
+tc_rgb = array(c(tc_red, tc_green, tc_blue, latitude, longitude), 
+               dim = c(nrow(tc_red), ncol(tc_red), 5))
+nc_rgb = array(c(nc_red, nc_green, nc_blue), 
+               dim = c(nrow(nc_red), ncol(nc_red), 3))
+
+## save jpegs
+  
+writename = paste0(date, '_', name, '_v', Sys.Date(), '.jpg')
+
+writeJPEG(tc_rgb, file.path(dumpdir, paste0('tc_', writename)), quality =1)
+writeJPEG(nc_rgb, file.path(dumpdir, paste0('nc_', writename)), quality =1)
+
+nc_close(viirs)
--- VIIRS_nc_jpg.py
+++ VIIRS_nc_jpg.py
@@ -1,18 +1,65 @@
 # import modules
-
+import os
 import matplotlib.pyplot as plt
 import netCDF4 as nc
+import numpy as np
+from PIL import Image
+
 
 #point to directory
 dir = '/Users/steeleb/OneDrive - Colostate/Superior/images/LAADS_VIIRS_nc/'
 
+#get a list of files here
+files = os.listdir(dir)
+#and look at the list to make sure it seems right
+print(files)
+
 #for now, work with one file
-file = dir + 'VJ102IMG.A2018091.1942.021.2021023002114.nc'
+file = dir + files[4]
 
 #read in as netcdf; xarray will not recognize the dimensions
-viirs = nc.Dataset(file)
+viirs = nc.Dataset(file, 'r')
+
+print(viirs)
 
 #store the band group in it's own object
 viirs_bands = viirs['observation_data']
-viirs_bands['I01']
+#store the image geolocs in their own object
+viirs_locs = viirs['scan_line_attributes']
 
+#view names of the variables
+print(viirs_bands.variables.keys())
+
+#VIIRS natural rgb are M10, M7, M5 - grab their masked layers
+v_natred = viirs_bands.variables['M10'][:]
+v_natgreen = viirs_bands.variables['M07'][:]
+v_natblue = viirs_bands.variables['M05'][:]
+#apply the mask to the layer
+v_natred = v_natred.filled(0)
+v_natgreen = v_natgreen.filled(0)
+v_natblue = v_natblue.filled(0)
+
+#True color rgb is M5, M4, M3
+v_truered = viirs_bands.variables['M05'][:]
+v_truegreen = viirs_bands.variables['M04'][:]
+v_trueblue = viirs_bands.variables['M03'][:]
+#apply the mask to the layer
+v_truered = v_truered.filled(0)
+v_truegreen = v_truegreen.filled(0)
+v_trueblue = v_trueblue.filled(0)
+
+#composite into rgb
+v_nat_rgb = np.dstack((v_natred, v_natgreen, v_natblue))
+v_true_rgb = np.dstack((v_truered, v_truegreen, v_trueblue))
+v_natimage = Image.fromarray(v_nat_rgb, 'RGB')
+v_trueimage = Image.fromarray(v_true_rgb, 'RGB')
+
+plt.imshow(v_natimage)
+plt.show()
+
+img = np.array(rgb_array, dtype=int).reshape((1, len(rgb_array), 3))
+plt.imshow(img, extent=[0, 16000, 0, 1], aspect='auto')
+plt.show()
+
+v_natimage.save('viirs_nat.jpg')
+v_trueimage.save('viirs_true.jpg')
